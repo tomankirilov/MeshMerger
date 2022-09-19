207676909b8c39e1b8cc853fafd4274b441adb5d
tomanski <toman@posteo.eu>
2022-09-19 19:10:43 +0300

    Updated to V4 of Godot

--- MeshMerger.gd
+++ MeshMerger.gd
@@ -1,36 +1,53 @@
-extends MeshInstance
+extends MeshInstance3D
 class_name MeshMerger
-tool
+@tool
 
 var result_mesh:ArrayMesh
 var result_mesh_material:Material
 
 
-export (NodePath) var collision_parent_path
-onready var collision_parent:Node
+@export  var collision_parent_path:NodePath
+@onready var collision_parent:Node
 
-export (bool) var btn_merge_meshes setget merge_meshes
-export (bool) var btn_clean_meshes setget clean_meshes
 
-export (bool) var toggle_children_visibility setget set_toggle_children_visibility
-export (bool) var delete_child_meshes_on_play setget set_delete_child_meshes_on_play
+@export var btn_merge_meshes:bool = false:
+	set(value):
+		merge_meshes()
+
+@export var btn_clean_meshes:bool = false:
+	set(value):
+		clean_meshes()
+		
+		
+@export var toggle_children_visibility:bool = false:
+	get:
+		return toggle_children_visibility
+	set(value):
+		toggle_children_visibility = value
+		set_toggle_children_visibility(value)
+
+@export var delete_child_meshes_on_play:bool = false:
+	set(value):
+		delete_child_meshes_on_play = value
+	get:
+		return delete_child_meshes_on_play
 
 # warning-ignore:unused_argument
-func merge_meshes(value):
-	if !Engine.editor_hint:
+func merge_meshes() -> void:
+	if !Engine.is_editor_hint():
 		return
 
-	if result_mesh:
+	if result_mesh != null:
 		result_mesh.clear_surfaces()
 	
-	if collision_parent_path:
+	if collision_parent_path != null:
 		collision_parent = get_node(collision_parent_path)
 		clean_collisions()
 
 	var _surface_tool := SurfaceTool.new()
 
 	for node in get_children():
-		if node is MeshInstance:
+		if node is MeshInstance3D:
 			_surface_tool.append_from(node.mesh, 0, node.transform)
 			grab_material(node)
 			generate_collisions(node)
@@ -42,8 +59,8 @@ func merge_meshes(value):
 	self.material_override = result_mesh_material
 
 # warning-ignore:unused_argument
-func clean_meshes(value):
-	if !Engine.editor_hint:
+func clean_meshes() -> void:
+	if !Engine.is_editor_hint():
 		return
 
 	if collision_parent_path:
@@ -54,44 +71,40 @@ func clean_meshes(value):
 	set_toggle_children_visibility(true)
 
 
-func grab_material(node):
-	if node.get_surface_material(0):
-		result_mesh_material = node.get_surface_material(0)
-	elif node.mesh.surface_get_material(0):
-		result_mesh_material = node.mesh.surface_get_material(0)
+func grab_material(node) -> void:
+	if node.get_active_material(0) != null:
+		result_mesh_material = node.get_active_material(0)
+	elif node.mesh._surface_get_material(0) != null:
+		result_mesh_material = node.mesh._surface_get_material(0)
 
-func generate_collisions(node):
-	if !collision_parent or !collision_parent_path:
+func generate_collisions(node) -> void:
+	if collision_parent == null or collision_parent_path == null:
 		return
 	for child in node.get_children():
-		if child is StaticBody and child.get_child_count() > 0:
+		if child is StaticBody3D and child.get_child_count() > 0:
 			for grandchild in child.get_children():
-				if grandchild is CollisionShape:
-					var new_col := CollisionShape.new()
+				if grandchild is CollisionShape3D:
+					var new_col := CollisionShape3D.new()
 					new_col.global_transform = child.global_transform
 					collision_parent.add_child(new_col)
 					new_col.shape = grandchild.shape
 					new_col.set_owner(get_tree().get_edited_scene_root())
 
-func clean_collisions():
-	if !collision_parent or !collision_parent_path:
+func clean_collisions() -> void:
+	if collision_parent == null or collision_parent_path == null:
 		return
 	if collision_parent.get_child_count() > 0:
 		for child in collision_parent.get_children():
 			child.queue_free()
 
-func set_toggle_children_visibility(value):
-	toggle_children_visibility = value
+func set_toggle_children_visibility(value) -> void:
 	for node in get_children():
-		if node is MeshInstance:
-			node.visible = value
+		if node is MeshInstance3D:
+			node.set_visible(value)
 
-func set_delete_child_meshes_on_play(value):
-	delete_child_meshes_on_play = value
-
-
-func _ready():
+func _ready() -> void:
 	if delete_child_meshes_on_play:
 		for node in get_children():
-			if node is MeshInstance:
+			if node is MeshInstance3D:
+				print("children deleted")
 				node.queue_free()
